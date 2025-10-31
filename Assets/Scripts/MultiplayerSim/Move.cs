ec65d92aca2763ff3adf8e46b1f482155f3dc475
Yannek <yannekzessin@gmail.com>
2025-10-31 10:37:10 +0100

    chore: added some models

---
+++ Assets/Scripts/MultiplayerSim/Move.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float radius;
+    [SerializeField] private Vector3 center;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
