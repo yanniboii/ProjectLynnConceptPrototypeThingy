9cf88bb3dc65532ee3e274f521f2d033c513dfec
Yannek <yannekzessin@gmail.com>
2025-11-03 14:53:47 +0100

    feat: finished the move script

--- Assets/Scripts/MultiplayerSim/Move.cs
+++ Assets/Scripts/MultiplayerSim/Move.cs
@@ -2,18 +2,38 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    [SerializeField] private Transform center;
     [SerializeField] private float radius;
-    [SerializeField] private Vector3 center;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
+    [SerializeField] Vector3 newPos = new Vector3();
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (newPos == Vector3.zero)
+            GetNewPos();
 
+        //transform.position = Vector3.Slerp(transform.position, newPos, moveSpeed);
+
+
+        Quaternion targetRotation = Quaternion.LookRotation(transform.position - newPos);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed);
+
+        transform.Translate(moveSpeed * Vector3.back, Space.Self);
+
+        if (Mathf.Abs(Vector3.Distance(transform.position, newPos)) < 0.1f)
+            newPos = new Vector3();
+    }
+
+    private void GetNewPos()
+    {
+        newPos = new Vector3(Random.Range(-radius, radius),
+                            Random.Range(-radius, radius),
+                            Random.Range(-radius, radius));
+
+        newPos += center.position;
     }
 }
